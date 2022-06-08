e4a4e1d8f231ef6b7cd422801ef79936b8ba8336
JOMA-OFFICE <jomadelema@hotmail.com>
2022-06-08 12:27:30 -0600

    se mejoró la interfaz para dispositivos moviles

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -4,7 +4,7 @@ const Contenedor = ({ titulo, altura, ancho, fondo, children, elevacion }) => {
 
     return (
        <>
-            <div className={`rounded-sm p-5 bg-white ${fondo} ${altura} ${"shadow-"+elevacion} m-5 ${ancho} dark:border dark:border-gray-700` }>
+            <div className={`rounded-sm p-5 bg-white ${fondo} ${altura} ${"shadow-"+elevacion} m-5 ${ancho} dark:border dark:border-gray-700 ` }>
                 <div className='text-3xl text-black dark:text-gray-200 font-bold text-center mb-5'>{titulo}</div>
                 <div>
                     {children}
