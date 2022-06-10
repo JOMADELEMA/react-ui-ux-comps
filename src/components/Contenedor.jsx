4ad6e3ddd120ecc86a6f426293fe4c7cc2414285
JOMA-OFFICE <jomadelema@hotmail.com>
2022-06-10 16:33:49 -0600

    se empezo a trabajar con responsividad de tarjetas

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -4,7 +4,7 @@ const Contenedor = ({ titulo, altura, ancho, children }) => {
 
     return (
        <>
-            <div className={`rounded-sm p-5 ${altura} m-5 ${ancho} ` }>
+            <div className={`p-5 ${altura} m-5 ${ancho}` }>
                 <div className='text-3xl text-black dark:text-gray-200 font-bold text-center mb-5'>{titulo}</div>
                 <div>
                     {children}
